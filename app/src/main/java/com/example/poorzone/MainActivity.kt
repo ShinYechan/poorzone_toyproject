34e19f2c37c8de1ebf0b74394eb0c26fc06b49bb
syc <yechan19202127@gmail.com>
2023-07-13 15:51:47 +0900

    bottom navigation bar 추가

--- app/src/main/java/com/example/poorzone/MainActivity.kt
+++ app/src/main/java/com/example/poorzone/MainActivity.kt
@@ -10,5 +10,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        init()
+    }
+
+    fun init(){
+        val nav = binding.bnvMain
+
+        nav.run {
+            setOnItemSelectedListener {
+                when(it.itemId){
+                    R.id.first->{
+                        val moneyTipFragment = MoneyTipFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container, moneyTipFragment).commit()
+                    }
+                    R.id.second->{
+                        val homeFragment = HomeFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container, homeFragment).commit()
+                    }
+                    R.id.third->{
+                        val mypageFragment = MypageFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container, mypageFragment).commit()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.second
+        }
     }
 }
